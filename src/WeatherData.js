ed0d96c77eb41bcc27a031c4d49b300190ed64c5
M-Lina3 <117495861+M-Lina3@users.noreply.github.com>
2023-02-06 00:22:36 -0500

    added code mapping :3

--- src/WeatherData.js
+++ src/WeatherData.js
@@ -1,11 +1,65 @@
 import React from "react";
-import "./App.css"
+import "./App.css";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
-export default function WeatherData(){
+export default function WeatherData(props) {
+  return (
+    <div>
+      <div className="row mt-3 ms-1">
+        <div className="col-8">
+          <div className="eldate">
+            <FormattedDate date={props.data.date} />
+          </div>
+          <div className="city">{props.data.name}</div>
+          <div className="country">US</div>
+          <div className="maintemp">
+            {props.data.temperature}&nbsp;
+            <span className="smol">
+              <span>°F</span> | <span>°C</span>
+            </span>
+          </div>
+          <div className="feels">Feels Like: {props.data.feels}°</div>
+          <div className="highlow">
+            {props.data.min}°/{props.data.max}°
+          </div>
+          <div>
+            <WeatherIcon code={props.data.icon} />
+           
+          </div>
+          <div className="text-capitalize">{props.data.description}</div>
+          <div>Wind: {props.data.wind} m/sec</div>
+          <div>Humidity: {props.data.humidity}%</div>
+        </div>
 
-    return (<div>
-        
-        
-        "Nya"</div>);
+        <div className="col-4 forecat">
+          <div className="row pb-2 ms-1">
+            <div>Monday</div>
+            <div>🌤</div>
+            <div>33°/33°</div>
+          </div>
+          <div className="row pb-2 ms-1">
+            <div>Tuesday</div>
+            <div>🌤</div>
+            <div>33°/33°</div>
+          </div>
+          <div className="row pb-2 ms-1">
+            <div>Wednesday</div>
+            <div>🌤</div>
+            <div>33°/33°</div>
+          </div>
+          <div className="row pb-2 ms-1">
+            <div>Thursday</div>
+            <div>🌤</div>
+            <div>33°/33°</div>
+          </div>
+          <div className="row pb-2 ms-1">
+            <div>Friday</div>
+            <div>🌤</div>
+            <div>33°/33°</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import "./App.css";
 import axios from "axios";
-import FormattedDate from "./FormattedDate"
 import WeatherData from "./WeatherData";
 
 export default function WeatherSearch(props) {
   const [weather, setWeather] = useState({ live: false });
+  const [city, setCity] = useState(props.defaultCity);
 
   function displayWeather(response) {
     setWeather({
@@ -13,12 +13,31 @@ export default function WeatherSearch(props) {
       date: new Date(response.data.dt * 1000),
       country: response.data.sys.country,
       temperature: Math.round(response.data.main.temp),
+      feels: Math.round(response.data.main.feels_like),
+      min: Math.round(response.data.main.temp_min),
+      max: Math.round(response.data.main.temp_max),
       wind: response.data.wind.speed,
       name: response.data.name,
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: response.data.weather[0].icon,
+      //`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     });
+  }
+
+  function search() {
+    const apiKey = "0b6316953363b3ec3e7536039a47981b";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function searchCity(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function cityChange(event) {
+    setCity(event.target.value);
   }
 
   if (weather.live) {
@@ -28,136 +47,21 @@ export default function WeatherSearch(props) {
           <div className="col"></div>
         </div>
 
-        <form className="laform">
+        <form className="laform" onSubmit={searchCity}>
           <input
             type="search"
             placeholder="Enter a city..."
             className="searchbar"
+            onChange={cityChange}
           />
           <input type="Submit" value="Search" className="elbutton ms-1" />
           <button className="elbutton ms-2">🌟</button>
         </form>
-<WeatherData />
-        <div className="row mt-3 ms-1">
-          <div className="col-8">
-            <div className="eldate">
-              <FormattedDate date={weather.date} />
-            </div>
-            <div className="city">{weather.name}</div>
-            <div className="country">US</div>
-            <div className="maintemp">
-              {weather.temperature}&nbsp;
-              <span className="smol">
-                <span>°F</span> | <span>°C</span>
-              </span>
-            </div>
-            <div className="feels">Feels Like: 48°</div>
-            <div className="highlow">50°/70°</div>
-            <div>
-              <img
-                src={weather.icon}
-                alt={weather.description}
-                className="mainicon"
-              ></img>
-            </div>
-            <div className="text-capitalize">{weather.description}</div>
-            <div>Wind: {weather.wind} m/sec</div>
-            <div>Humidity: {weather.humidity}%</div>
-          </div>
-
-          <div className="col-4 forecat">
-            <div className="row pb-2 ms-1">
-              <div>Monday</div>
-              <div>🌤</div>
-              <div>33°/33°</div>
-            </div>
-            <div className="row pb-2 ms-1">
-              <div>Tuesday</div>
-              <div>🌤</div>
-              <div>33°/33°</div>
-            </div>
-            <div className="row pb-2 ms-1">
-              <div>Wednesday</div>
-              <div>🌤</div>
-              <div>33°/33°</div>
-            </div>
-            <div className="row pb-2 ms-1">
-              <div>Thursday</div>
-              <div>🌤</div>
-              <div>33°/33°</div>
-            </div>
-            <div className="row pb-2 ms-1">
-              <div>Friday</div>
-              <div>🌤</div>
-              <div>33°/33°</div>
-            </div>
-          </div>
-        </div>
+        <WeatherData data={weather} />
       </div>
     );
   } else {
-    const apiKey = "0b6316953363b3ec3e7536039a47981b";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&units=metric&appid=${apiKey}`;
-    axios.get(apiUrl).then(displayWeather);
-
+    search();
     return "Nyaaa...";
   }
-
-  /*
-  const [city, setCity] = useState("");
-  const [loaded, setLoaded] = useState(false);
-  const [weather, setWeather] = useState({});
-
-  function obtainWeather(event) {
-    event.preventDefault();
-    let apiKey = "0b6316953363b3ec3e7536039a47981b";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-    axios.get(apiUrl).then(displayWeather);
-  }
-
-  function displayWeather(response) {
-    setLoaded(true);
-    setWeather({
-      temperature: response.data.main.temp,
-      wind: response.data.wind.speed,
-      humidity: response.data.main.humidity,
-      description: response.data.weather[0].description,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-    });
-  }
-
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
-  let form = (
-    <form onSubmit={obtainWeather}>
-      <input
-        type="search"
-        placeholder="Enter a city..."
-        onChange={updateCity}
-      />
-      <input type="Submit" value="Search" />
-    </form>
-  );
-
-  if (loaded) {
-    return (
-      <div>
-        {form}
-        <div>
-          <ul>
-            <li>Temperature: {Math.round(weather.temperature)}℃</li>
-            <li>Humidity: {weather.humidity}%</li>
-            <li>Wind: {weather.wind}m/sec</li>
-            <li>Description: {weather.description}</li>
-            <li>
-              <img src={weather.icon} alt={weather.description} />
-            </li>
-          </ul>
-        </div>
-      </div>
-    );
-  } else {
-    return form;
-  }*/
 }
